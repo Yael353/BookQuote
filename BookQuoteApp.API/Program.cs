c56974d2891d1a4d4be6df95acc6a6ededd8f892
Yaser Elkhabiry <yaser.elkhabiry@outlook.com>
2026-05-22 13:21:34 +0200

    config with correct port binding

--- BookQuoteApp.API/Program.cs
+++ BookQuoteApp.API/Program.cs
@@ -2,26 +2,19 @@ using BookQuoteApp.API.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
-
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=bookquoteapp.db"));
 
-
+// JWT
 var jwtKey = builder.Configuration["Jwt:Key"] ?? "minSuperHemligaNyckelSomÄrMinst32TeckenLång!2025";
 var key = Encoding.UTF8.GetBytes(jwtKey);
-
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -34,33 +27,29 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = new SymmetricSecurityKey(key)
         };
     });
-
 builder.Services.AddAuthorization();
+
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAngular",
+    options.AddPolicy("AllowAll",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200", "https://bookquoteapp.vercel.app")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
         });
 });
 
 var app = builder.Build();
 
-
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-//app.UseHttpsRedirection();
-app.UseCors("AllowAngular");   
-app.UseAuthentication();       
-app.UseAuthorization();        
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors("AllowAll");
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
-app.Urls.Add($"http://*:{port}");
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+app.Run($"http://0.0.0.0:{port}");
